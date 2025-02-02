0b4891dd96d9476fc8d4609314a302ee4cc0af4d
SamuelTobio <sleonardot@gmail.com>
2025-02-01 22:09:27 -0500

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -98,10 +98,14 @@ export function loadProductsFetch() {
       });
 
       console.log("load products");
+    }).catch((error) => {
+      console.log('Unexpected error. Please try again later.');
     });
 
     return promise; 
 }
+
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step'); 
@@ -123,9 +127,17 @@ export function loadProducts(fun) {
 
     fun();
   });
+
+
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.'); 
+  })
+
   xhr.open("GET", "https://supersimplebackend.dev/products");
   xhr.send();
 }
+loadProducts();
 
 /*
 export const products = [
